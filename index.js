961b65562530043b62b1ac5c1c6cc79c885817f0
Unknown <matt.bertoncello@gmail.com>
2019-04-04 17:04:32 +1100

    Connect to MongoDB

--- index.js
+++ index.js
@@ -2,36 +2,37 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const {Pool} = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+var mongodb = require('mongodb')
+
+require('dotenv').config();
+
+const mongoDetails = {
+  dbName: process.env.MONGO_DB,
+  user: process.env.MONGO_USER,
+  password: process.env.MONGO_PASSWORD,
+  cluster: process.env.MONGO_CLUSTER
+}
+
+const uri = 'mongodb+srv://'+mongoDetails.user+':'+mongoDetails.password+'@'+mongoDetails.cluster+'-clgtv.gcp.mongodb.net/'+mongoDetails.dbName+'?authSource=admin&retryWrites=true';
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Use connect method to connect to the server
+MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(mongoDetails.dbName);
+
+  client.close();
 });
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
   .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => {
-    let result = ''
-    const times = process.env.TIMES || 5
-    for (i=0; i<times; i++) {
-      result += i +' '
-    }
-    res.send(result)
-  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
