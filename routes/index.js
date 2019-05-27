6aac7e1265b156ad8ee453aaeb6b8225595e0cdd
matt-bertoncello <matt.bertoncello@gmail.com>
2019-05-27 15:57:22 +1000

    If user is already logged in, redirect to /user page on /login

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,19 @@ var router = express.Router();
 
 router.get('/', (req, res) => {req.session.host = req.hostname; res.render('index', {req: req});})
 router.get('/session', (req, res) => res.render('session', {req: req}))
-router.get('/login', (req, res, next) => res.render('login', {req: req}))
 router.get('/register', (req, res) => res.render('register', {req: req}))
 router.post('/register', (req, res) => auth_controller.doRegister(req, res))
+
+/* LOGIN capabilities. If user is already logged in, redirect to user page. */
 router.post('/login', (req, res) => auth_controller.doLogin(req, res))
+router.get('/login', (req, res, next) => {
+  if(req.session.passport && req.session.passport.user) {
+    console.log('[ERROR] '+req.session.passport.user.email+" is already logged in");
+    res.redirect('/user');
+  } else {
+    res.render('login', {req: req})
+  }
+})
 
 function updateSession(req, res, next) {
   next();
