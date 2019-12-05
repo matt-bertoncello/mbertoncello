53b7901997d1dc65071c1e9cb9f80e98d1fd5819
matt-bertoncello <matt.bertoncello@gmail.com>
2019-12-05 23:06:38 +1100

    Replaced bcrypt package with bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 var passportLocalMongoose = require('passport-local-mongoose');
 var SALT_WORK_FACTOR = 10;
 
